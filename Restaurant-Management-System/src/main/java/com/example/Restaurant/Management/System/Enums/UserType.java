2d92a6079c37dd7544146cc3e248b003e54282e9
Sajjan Pandey <sajjan2vp@gmail.com>
2025-06-21 11:32:13 +0530

    Create folder structure

---
+++ Restaurant-Management-System/src/main/java/com/example/Restaurant/Management/System/Enums/UserType.java
@@ -0,0 +1,4 @@
+package com.example.Restaurant.Management.System.Enums;
+
+public enum UserType {
+}
---
+++ Restaurant-Management-System/src/main/java/com/example/Restaurant/Management/System/Security/Config.java
@@ -0,0 +1,4 @@
+package com.example.Restaurant.Management.System.Security;
+
+public class Config {
+}
